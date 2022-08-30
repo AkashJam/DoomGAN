b07cf15fecb9047c439ef5c5dbc4ff096282e220
AkashJam <akashaloysius.james@gmail.com>
2022-08-30 23:01:08 +0200

    Iterative WAD scrapping

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,20 +1,21 @@
+import urllib.request
 import requests
 from bs4 import BeautifulSoup
 
+session = requests.Session()
 def open_page(url):
-    session = requests.Session()
     page = session.get(url).text
     return BeautifulSoup(page, "lxml")
 
-repo_link = "https://www.doomworld.com/idgames/levels/doom/"
+repo_link = 'https://www.doomworld.com/idgames/levels/doom/'
 # list of subcategories to be avoided
-excluded_list = ["deathmatch","Ports","megawads"]
+excluded_list = ['deathmatch','Ports','megawads']
 level_page = open_page(repo_link)
 # Fetching subcategory url in doomworld 
 level_page_links = level_page.find_all('a')
 for l in level_page_links:
     # Finding urls that contain this path
-    if l.has_attr('href') and "idgames/levels/doom" in l['href']:
+    if l.has_attr('href') and 'idgames/levels/doom' in l['href']:
         cate_path = l['href']
         
         # Skipping excluded categories
@@ -29,25 +30,43 @@ for l in level_page_links:
         cate_page = open_page(cate_url)
         cate_page_links = cate_page.find_all('a')
         for c in cate_page_links:
-            if c.has_attr('href') and c['href'].startswith("levels/doom"):
+            if c.has_attr('href') and c['href'].startswith('levels/doom'):
                 map_path = c['href']
                 map_id = c['href'].split('/')[-1]
-                # print(map_id,map_path)
 
                 # Fetching download links of WAD files
                 found_files = []
                 map_url = cate_url + map_id
-                print ("Opening category page: " + map_url)
+                print ('Opening map page: ' + map_url)
                 map_page = open_page(map_url)
-                # print(map_page)
-                # Find the div containing the pagination design pattern and the file links
-                mainDiv = map_page.findAll("table", {"class": "download"})[0]
-                file_page_links = [entry.find_all("a")[0] for entry in mainDiv.find_all("ul", {"class":"square"})]
-                for entry in mainDiv.find_all("ul", {"class":"square"}):
-                    print("entries",entry)
-                found_files = found_files + [fpl['href'] for fpl in file_page_links]
-                print("Found {} more files".format(str(len(found_files))))
-                print(found_files)
+                # Finds the table containing the WAD download links
+                mainDiv = map_page.findAll('table', {'class': 'download'})[0]
+                file_page_links = [entry.find_all('a') for entry in mainDiv.find_all('ul', {'class':'square'})][0]
+                filename = file_page_links[0]['href'].split('/')[-1]
+                for file in file_page_links:
+                    print(file['href'])
+                    link = file['href']
+                    if link[0] =='f':
+                        try:
+                            # for ftp servers
+                            urllib.request.urlretrieve(file['href'], '../dataset/scraped/doom/' + filename)
+                        except Exception as e:
+                            print(e)
+                    else:
+                        r = session.get(link)
+                        if r.status_code != 200:
+                            print ('Failed to download the file: ' + link)
+                        # filename = r.url.split('/')[-1]
+                        with open('../dataset/scraped/doom/' + filename, 'wb') as file:
+                            file.write(r.content)
+                    break
+                break
+        break
+                    # print("LINK",file['href'])
+                    # break
+                # found_files = found_files + [fpl['href'] for fpl in file_page_links]
+                # print("Found {} more files".format(str(len(found_files))))
+                # print(found_files)
 
 # for ftp servers
 # urllib.request.urlretrieve('ftp://ftp.fu-berlin.de/pc/games/idgames/levels/doom/0-9/0.zip', './scraped/doom/0.zip')
@@ -60,5 +79,5 @@ for l in level_page_links:
 # if r.status_code != 200:
 #     print ("Failed to download the file: " + url)
 # filename = r.url.split('/')[-1]
-# with open("./"+filename, "wb") as file:
+# with open("./scraped/doom/"+filename, "wb") as file:
 #     file.write(r.content)
